0665e4c7a0270477cc20f229d8f34d7d5fe50f90
Connor Taffe <cpaynetaffe@gmail.com>
2014-06-09 15:20:44 -0500

    Tool API work & multiple model loads work

--- mesher/js/toolv2.js
+++ mesher/js/toolv2.js
@@ -5,17 +5,39 @@
 // & parsing tool declarations.
 
 (function (m$) {
-	m$.Toool = function () {
 
-		// Specs for tool
-		this.hasUndo = false;
-		this.Name = "default";
-
-		// Map of parameters
-		this.Params = {};
-
-		// Functions to call
-		this.do = function () { /* function */ };
-		this.undo = function () { /* function */ };
+	// Array of Tool types (tools)
+	m$.Tool = function () {
+		this.Tools = [];
 	};
+
+	m$.Tool.prototype.New = function (name, do, undo, toString, check, prepare) {
+
+		var tool = function (project) {
+			this.Params = aruguments;
+			this.Project = project;
+		}
+
+		// set undo function
+		if (undo === false){
+			tool.prototype.undo = function () { return false; };
+		} else {
+			tool.prototype.undo = undo;
+		}
+
+		// sets name
+		tool.name = name;
+
+		// Does the transformation
+		tool.prototype.do = do;
+
+		// Returns a string like "Transform(12, 2)"
+		tool.prototype.toString = toString;
+
+		tool.prototype.check = check;
+		tool.prototype.prepare = prepare;
+
+		this.Tools.push(tool);
+	};
+
 })(Mesher);
