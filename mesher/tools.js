8919cc54f327fa4dd67750cfb0933b6a94624545
Connor Taffe <cpaynetaffe@gmail.com>
2014-07-20 11:38:31 -0500

    Added autocenter, do & redo now share a tool-wide function for many tools

--- mesher/tools.js
+++ mesher/tools.js
@@ -126,18 +126,21 @@
 		name: "Shift",
 		icon: "fa-arrows",
 		do: function () {
-			var scalar = this.Params['scalar'];
 			this.models = [];
+			this.Shift= function () {
+				var scalar = this.Params['scalar'];
+				for (var i = 0; i < this.models.length; i++) {
+					model = this.models[i];
+					if (this.Params['x']){model.translateX(scalar);}
+					if (this.Params['y']){model.translateY(scalar);}
+					if (this.Params['z']){model.translateZ(scalar);}
+				}
+				return true;
+			}
 			for (var i = 0; i < this.Project.SelectedModels.length; i++) {
 				this.models.push(this.Project.SelectedModels[i]);
 			}
-			for (var i = 0; i < this.models.length; i++) {
-				model = this.models[i];
-				if (this.Params['x']){model.translateX(scalar);}
-				if (this.Params['y']){model.translateY(scalar);}
-				if (this.Params['z']){model.translateZ(scalar);}
-			}
-			return true;
+			return this.Shift();
 		},
 		undo: function () {
 			if (typeof this.models == 'undefined'){return false;}
@@ -152,14 +155,7 @@
 		},
 		redo: function () {
 			if (typeof this.models == 'undefined'){return false;}
-			var scalar = this.Params['scalar'];
-			for (var i = 0; i < this.models.length; i++) {
-				model = this.models[i];
-				if (this.Params['x']){model.translateX(scalar);}
-				if (this.Params['y']){model.translateY(scalar);}
-				if (this.Params['z']){model.translateZ(scalar);}
-			}
-			return true;
+			return this.Shift();
 		},
 		check: function (map) {
 			return (map['project'].SelectedModels.length > 0);
@@ -196,38 +192,39 @@
 		name: "Rotate",
 		icon: "fa-undo",
 		do: function () {
-			var scalar = this.Params['scalar'];
 			this.models = [];
+			this.Rotate = function () {
+				var scalar = this.Params['scalar'];
+				var rotx, roty, rotz = {};
+				if (this.Params['x']){
+					rotx = new THREE.Matrix4().makeRotationX(Math.PI*scalar/180);
+				} else {
+					rotx = new THREE.Matrix4().makeRotationX(0);
+				}
+				if (this.Params['y']){
+					roty = new THREE.Matrix4().makeRotationY(Math.PI*scalar/180);
+				} else {
+					roty = new THREE.Matrix4().makeRotationY(0);
+				}
+				if (this.Params['z']){
+					rotz = new THREE.Matrix4().makeRotationZ(Math.PI*scalar/180);
+				} else {
+					rotz = new THREE.Matrix4().makeRotationZ(0);
+				}
+
+				for (var i = 0; i < this.models.length; i++) {
+					model = this.models[i];
+					model.geometry.applyMatrix(rotx.multiply(roty).multiply(rotz));
+					model.geometry.computeFaceNormals();
+					model.geometry.verticesNeedUpdate = true;
+					model.geometry.normalsNeedUpdate = true;
+				}
+				return true;
+			}
 			for (var i = 0; i < this.Project.SelectedModels.length; i++) {
 				this.models.push(this.Project.SelectedModels[i]);
 			}
-
-			// Rotate matrixes
-			var rotx, roty, rotz = {};
-			if (this.Params['x']){
-				rotx = new THREE.Matrix4().makeRotationX(Math.PI*scalar/180);
-			} else {
-				rotx = new THREE.Matrix4().makeRotationX(0);
-			}
-			if (this.Params['y']){
-				roty = new THREE.Matrix4().makeRotationY(Math.PI*scalar/180);
-			} else {
-				roty = new THREE.Matrix4().makeRotationY(0);
-			}
-			if (this.Params['z']){
-				rotz = new THREE.Matrix4().makeRotationZ(Math.PI*scalar/180);
-			} else {
-				rotz = new THREE.Matrix4().makeRotationZ(0);
-			}
-
-			for (var i = 0; i < this.models.length; i++) {
-				model = this.models[i];
-				model.geometry.applyMatrix(rotx.multiply(roty).multiply(rotz));
-				model.geometry.computeFaceNormals();
-				model.geometry.verticesNeedUpdate = true;
-				model.geometry.normalsNeedUpdate = true;
-			}
-			return true;
+			return this.Rotate();
 		},
 		undo: function () {
 			var scalar = this.Params['scalar'];
@@ -261,34 +258,8 @@
 			return true;
 		},
 		redo: function () {
-			var scalar = this.Params['scalar'];
-			// Rotate matrixes
-			var rotx, roty, rotz = {};
-			if (this.Params['x']){
-				rotx = new THREE.Matrix4().makeRotationX(Math.PI*scalar/180);
-			} else {
-				rotx = new THREE.Matrix4().makeRotationX(0);
-			}
-			if (this.Params['y']){
-				roty = new THREE.Matrix4().makeRotationY(Math.PI*scalar/180);
-			} else {
-				roty = new THREE.Matrix4().makeRotationY(0);
-			}
-			if (this.Params['z']){
-				rotz = new THREE.Matrix4().makeRotationZ(Math.PI*scalar/180);
-			} else {
-				rotz = new THREE.Matrix4().makeRotationZ(0);
-			}
-
-			for (var i = 0; i < this.models.length; i++) {
-				model = this.models[i];
-				model.geometry.applyMatrix(rotx.multiply(roty).multiply(rotz));
-				model.geometry.computeFaceNormals();
-				model.geometry.verticesNeedUpdate = true;
-				model.geometry.normalsNeedUpdate = true;
-			}
-
-			return true;
+			if (typeof this.models == 'undefined'){return false;}
+			return this.Rotate();
 		},
 		check: function (map) {
 			return (map['project'].SelectedModels.length > 0);
@@ -325,66 +296,46 @@
 		name: "Slice",
 		icon: "fa-cube",
 		do: function () {
-			var scalar = this.Params['scalar'];
 			this.models = [];
+			this.Slice = function () {
+				var scalar = this.Params['scalar'];
+				for (var i = 0; i < this.models.length; i++) {
+					model = this.models[i];
+					model.geometry.computeBoundingBox();
+					var b = model.geometry.boundingBox;
+					//experiments in slicing
+					var plane_geometry = new THREE.CubeGeometry( 1000, 1000, 1000 );
+					var plane_mesh = new THREE.Mesh( plane_geometry );
+					if (this.Params['x']){plane_mesh.position.x = 500+b.min.x+scalar*(b.max.x - b.min.x)/100;}
+					if (this.Params['y']){plane_mesh.position.y = 500+b.min.y+scalar*(b.max.y - b.min.y)/100;}
+					if (this.Params['z']){plane_mesh.position.z = 500+b.min.z+scalar*(b.max.z - b.min.z)/100;}
+
+					var plane_bsp = new ThreeBSP( plane_mesh );
+					var cube_mesh = new THREE.Mesh( model.geometry );
+					var cube_bsp = new ThreeBSP( cube_mesh );
+					var result = cube_bsp.subtract(plane_bsp);
+					this.Project.Three.Scene.remove(model);
+					var material = model.material;
+					cube = result.toMesh(material);
+					model.geometry = cube.geometry;
+					this.Project.Three.Scene.add(cube);
+
+					model.geometry.verticesNeedUpdate = true;
+					model.geometry.normalsNeedUpdate=true;
+				}
+				return true;
+			}
 			for (var i = 0; i < this.Project.SelectedModels.length; i++) {
 				this.models.push(this.Project.SelectedModels[i]);
 			}
-			for (var i = 0; i < this.models.length; i++) {
-				model = this.models[i];
-				model.geometry.computeBoundingBox();
-				var b = model.geometry.boundingBox;
-				//experiments in slicing
-				var plane_geometry = new THREE.CubeGeometry( 1000, 1000, 1000 );
-				var plane_mesh = new THREE.Mesh( plane_geometry );
-				if (this.Params['x']){plane_mesh.position.x = 500+b.min.x+scalar*(b.max.x - b.min.x)/100;}
-				if (this.Params['y']){plane_mesh.position.y = 500+b.min.y+scalar*(b.max.y - b.min.y)/100;}
-				if (this.Params['z']){plane_mesh.position.z = 500+b.min.z+scalar*(b.max.z - b.min.z)/100;}
-
-				var plane_bsp = new ThreeBSP( plane_mesh );
-				var cube_mesh = new THREE.Mesh( model.geometry );
-				var cube_bsp = new ThreeBSP( cube_mesh );
-				var result = cube_bsp.subtract(plane_bsp);
-				this.Project.Three.Scene.remove(model);
-				var material = model.material;
-				cube = result.toMesh(material);
-				model.geometry = cube.geometry;
-				this.Project.Three.Scene.add(cube);
-
-				model.geometry.verticesNeedUpdate = true;
-				model.geometry.normalsNeedUpdate=true;
-			}
-			return true;
+			return this.Slice();
 		},
 		undo: function () {
 			return false;
 		},
 		redo: function () {
-			var scalar = this.Params['scalar'];
-			for (var i = 0; i < this.models.length; i++) {
-				model = this.models[i];
-				model.geometry.computeBoundingBox();
-				var b = model.geometry.boundingBox;
-				//experiments in slicing
-				var plane_geometry = new THREE.CubeGeometry( 1000, 1000, 1000 );
-				var plane_mesh = new THREE.Mesh( plane_geometry );
-				if (this.Params['x']){plane_mesh.position.x = 500+b.min.x+scalar*(b.max.x - b.min.x)/100;}
-				if (this.Params['y']){plane_mesh.position.y = 500+b.min.y+scalar*(b.max.y - b.min.y)/100;}
-				if (this.Params['z']){plane_mesh.position.z = 500+b.min.z+scalar*(b.max.z - b.min.z)/100;}
-
-				var plane_bsp = new ThreeBSP( plane_mesh );
-				var cube_mesh = new THREE.Mesh( model.geometry );
-				var cube_bsp = new ThreeBSP( cube_mesh );
-				var result = cube_bsp.subtract(plane_bsp);
-				this.Project.Three.Scene.remove(model);
-				var material = model.material;
-				cube = result.toMesh(material);
-				model.geometry = cube.geometry;
-				this.Project.Three.Scene.add(cube);
-
-				model.geometry.verticesNeedUpdate = true;
-				model.geometry.normalsNeedUpdate=true;
-			}
+			if (typeof this.models == 'undefined'){return false;}
+			return this.Slice();
 		},
 		check: function (map) {
 			return (map['project'].SelectedModels.length > 0);
@@ -504,6 +455,64 @@
 					index: map['index']
 				}));
 			}
+		}
+	});
+
+	// AutoCenter tool
+	m$.tool.New({
+		name: "Auto Center",
+		icon: "fa-arrows-alt",
+		do: function () {
+			this.models = [];
+			this.shift = [];
+			this.AutoCenter = function () {
+				for (var i = 0; i < this.models.length; i++) {
+					model = this.models[i];
+					var box = new THREE.Box3().setFromObject( model );
+					shiftX = -box.min.x;
+					shiftY = -box.min.y;
+					shiftZ = -box.min.z;
+					this.shift.push(shiftX);
+					this.shift.push(shiftY);
+					this.shift.push(shiftZ);
+					model.translateX(shiftX);
+					model.translateY(shiftY);
+					model.translateZ(shiftZ);
+				}
+				return true;
+			}
+
+			for (var i = 0; i < this.Project.SelectedModels.length; i++) {
+				this.models.push(this.Project.SelectedModels[i]);
+			}
+
+			return this.AutoCenter();
+		},
+		undo: function () {
+			if (typeof this.models == 'undefined'){return false;}
+			for (var i = 0; i < this.models.length; i++) {
+				model = this.models[i];
+				shiftZ = this.shift.pop()
+				shiftY = this.shift.pop()
+				shiftX = this.shift.pop()
+				model.translateX(-shiftX);
+				model.translateY(-shiftY);
+				model.translateZ(-shiftZ);
+			}
+			return true;
+		},
+		redo: function () {
+			if (typeof this.models == 'undefined'){return false;}
+			return this.AutoCenter();
+		},
+		check: function (map) {
+			return (map['project'].SelectedModels.length > 0);
+		},
+		prep: function (map) {
+			this.UIstack.push(new m$.HTML.List['ApplyInput'].New({
+				text: "Apply",
+				index: map['index']
+			}));
 		}
 	});
 
